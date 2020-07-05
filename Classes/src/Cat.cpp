4fef4c82a44cac97eb2035bb8fbad4ddf2df882c
Surajit Basak <surajitbasak109@gmail.com>
2020-07-06 00:18:01 +0530

    Introduction to Classes.

---
+++ Classes/src/Cat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak() {
+	cout << "Meouwwww!!!" << endl;
+}
+
+void Cat::jump() {
+	cout << "Jump to top of bookcase" << endl;
+}
---
+++ Classes/src/Cat.h
@@ -0,0 +1,17 @@
+/*
+ * Cat.h
+ *
+ *  Created on: 05-Jul-2020
+ *      Author: techcet
+ */
+
+#ifndef CAT_H_
+#define CAT_H_
+
+class Cat {
+public:
+	void speak();
+	void jump();
+};
+
+#endif /* CAT_H_ */
